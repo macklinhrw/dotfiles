22d16c086a0b1d0d6ff98f1adaae693e6df5e78b
macklin <macklingaming@gmail.com>
2022-06-07 01:01:49 -0700

    feat: new dots

---
+++ private_dot_config/polybar/executable_herbstluftwm.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+hash herbstclient xrandr
+
+print_tags() {
+	for tag in $(herbstclient tag_status "$1"); do
+		name=${tag#?}
+		state=${tag%$name}
+		case "$state" in
+		'#')
+			printf '%%{R} %s %%{R}' "$name"
+			;;
+		'+')
+			printf '%%{F#cccccc}%%{R} %s %%{R}%%{F-}' "%{A1:herbstclient use $name:}$name%{A}"
+			;;
+		'!')
+			printf '%%{R} %s! %%{R}' "%{A1:herbstclient use $name:}$name%{A}"
+			;;
+		':')
+			printf ' %s ' "%{A1:herbstclient use $name:}+$name%{A}"
+			;;
+		'.')
+			printf '%%{F#cccccc} %s %%{F-}' "%{A1:herbstclient use $name:}$name%{A}"
+			;;
+		*)
+			printf ' %s ' "%{A1:herbstclient use $name:}$name%{A}"
+		esac
+	done
+	printf '\n'
+}
+
+geom_regex='[[:digit:]]\+x[[:digit:]]\++[[:digit:]]\++[[:digit:]]\+'
+geom=$(xrandr --query | grep "^$MONITOR" | grep -o "$geom_regex")
+monitor=$(herbstclient list_monitors | grep "$geom" | cut -d: -f1)
+
+print_tags "$monitor"
+
+IFS="$(printf '\t')" herbstclient --idle | while read -r hook args; do
+	case "$hook" in
+	tag*)
+		print_tags "$monitor"
+		;;
+	esac
+done
